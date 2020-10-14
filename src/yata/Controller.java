02b3141c9a5182bde43734c26881e4f47a0a929f
schuub <34111742+schuub@users.noreply.github.com>
2020-10-14 17:06:21 +0200

    added dummy button handlers

--- src/yata/Controller.java
+++ src/yata/Controller.java
@@ -1,16 +1,28 @@
 package yata;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Controller {
 
     @FXML
-    private Label label;
+    private Label versionInfo;
+    public Label lbTimer;
+    public Button btnStart;
+    public Button btnSkip;
 
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Willkommen, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        versionInfo.setText("Willkommen, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        lbTimer.setText("00:00");
+    }
+    
+    public void handleStartClick() {
+    	System.out.println("Timer started");
+    }
+    public void handleSkipClick() {
+    	System.out.println("Phase skipped");
     }
 }
